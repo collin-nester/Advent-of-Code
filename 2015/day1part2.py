f27cf0ab2e7890c2edf944609423e0d86e579ea3
collin-nester <74081347+collin-nester@users.noreply.github.com>
2021-03-05 16:32:35 -0500

    Add files via upload

---
+++ 2015/day1part2.py
@@ -0,0 +1,19 @@
+#Advent of Code Day 1 Part 2 2015
+def main():
+    with open('day1input.txt', 'r') as reader:
+        bigList=reader.readlines()
+    letter=bigList[0]
+    x=0
+    y=0
+    z=0
+    for m in range(0,len(letter)):
+        if letter[m]=='(':
+            x=x+1
+        elif letter[m]==')':
+            x=x-1
+        if x==-1 and z!=-1:
+            z=-1
+            y=m+1
+    print('He reaches the basement at '+str(y))
+    
+main()
