834348a71542e961933ffa0d9cc1c443b019bc62
collin-nester <74081347+collin-nester@users.noreply.github.com>
2021-03-06 08:52:41 -0500

    Add files via upload

---
+++ 2015/day2part2.py
@@ -0,0 +1,30 @@
+#Advent of Code Day 2 Part 1
+def main():
+    with open('day2input.txt', 'r') as reader:
+        bigList=reader.readlines()
+    a=0
+    g=0
+    for y in bigList:
+        b=bigList[a].split('x')
+        c=b[0]
+        d=b[1]
+        e=b[2]
+        e=e.replace('\n','')
+        c=int(c)
+        d=int(d)
+        e=int(e)
+        h=2*c+2*d
+        i=2*d+2*e
+        j=2*c+2*e
+        f=c*d*e
+        if h<=i and h<=j:
+            g=h+g
+        elif i<=h and i<=j:
+            g=i+g
+        elif j<=h and j<=i:
+            g=j+g
+        g=g+f
+        a=a+1
+    print('The elves need '+str(g)+' feet of ribbon.')
+
+main()
