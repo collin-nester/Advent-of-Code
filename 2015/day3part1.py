39ce06b22b04a7ec6e1bf126bfdfffc145748e46
collin-nester <74081347+collin-nester@users.noreply.github.com>
2021-03-06 09:19:40 -0500

    Add files via upload

---
+++ 2015/day3part1.py
@@ -0,0 +1,26 @@
+#Advent of Code Day 3 Part 1
+def main():
+    with open('day3input.txt', 'r') as reader:
+        bigList=reader.readlines()
+    bigString=bigList[0]
+    emptyList=[]
+    a=0
+    b=0
+    e=0
+    for x in range(0,len(bigString)):
+        d=bigString[e]
+        if d=='^':
+            a=a+1
+        if d=='v':
+            a=a-1
+        if d=='>':
+            b=b+1
+        if d=='<':
+            b=b-1
+        c=(str(a)+','+str(b))
+        e=e+1
+        if c not in emptyList:
+            emptyList.append(c)
+    print(len(emptyList))
+
+main()
