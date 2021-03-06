39ebcb4cf86f4713ba296e0de7d9d87635c78f6c
collin-nester <74081347+collin-nester@users.noreply.github.com>
2021-03-06 11:34:00 -0500

    Add files via upload

---
+++ 2015/day4part1.py
@@ -0,0 +1,21 @@
+#Advent of Code Day 4 Part 1
+import hashlib
+def main():
+    with open('day4input.txt', 'r') as reader:
+        bigList=reader.readlines()
+    x=str(bigList)
+    a=x
+    c=1
+    d=0
+    while a[:5]!='00000':
+        b=x+str(c)
+        b=b.replace("['",'')
+        b=b.replace("']",'')
+        a=hashlib.md5(b.encode())
+        c=c+1
+        a=a.hexdigest()
+    print(b)
+    print(a)
+
+
+main()
